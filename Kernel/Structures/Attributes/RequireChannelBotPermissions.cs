90bd09e58101c3cb51edc6d0cfa3a734d82f9a9f
Hinoki Shiba <3950465+gspalato@users.noreply.github.com>
2020-04-19 18:49:03 -0300

    Fixed small problems.

---
+++ Kernel/Structures/Attributes/RequireChannelBotPermissions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Muon.Commands;
+using Qmmands;
+
+namespace Muon.Kernel.Structures.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class RequireChannelBotPermissionsAttribute : CheckAttribute
+	{
+		private ChannelPermission Value { get; }
+
+		public RequireChannelBotPermissionsAttribute(ChannelPermission permissions)
+		{
+			Value = permissions;
+		}
+
+		public override async ValueTask<CheckResult> CheckAsync(CommandContext _)
+		{
+			var context = (MuonContext)_;
+
+			var member = await context.Guild.GetCurrentUserAsync();
+
+			return member.GetPermissions(context.Channel).Has(Value)
+				? CheckResult.Successful
+				: CheckResult.Unsuccessful("You don't have enough permissions to do this.");
+		}
+	}
+}
---
+++ Kernel/Structures/Attributes/RequireChannelUserPermissions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Muon.Commands;
+using Qmmands;
+
+namespace Muon.Kernel.Structures.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class RequireChannelUserPermissionsAttribute : CheckAttribute
+	{
+		private ChannelPermission Value { get; }
+
+		public RequireChannelUserPermissionsAttribute(ChannelPermission permissions)
+		{
+			Value = permissions;
+		}
+
+		public override async ValueTask<CheckResult> CheckAsync(CommandContext _)
+		{
+			var context = (MuonContext)_;
+
+			IApplication app = await context.Client.GetApplicationInfoAsync();
+			if (app.Owner.Id == context.User.Id)
+				return CheckResult.Successful;
+
+			var member = await context.Guild.GetUserAsync(context.User.Id);
+
+			return member.GetPermissions(context.Channel).Has(Value)
+				? CheckResult.Successful
+				: CheckResult.Unsuccessful("You don't have enough permissions to do this.");
+		}
+	}
+}
