d8eeaaf235f3cbde1f2a791971dc3f21ea7b5e7a
Hinoki Shiba <3950465+gspalato@users.noreply.github.com>
2020-05-11 23:13:36 -0300

    Created error message suppressing.

---
+++ Axion.Core/Structures/Attributes/SupressPermissionErrorsAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Axion.Core.Structures.Attributes
+{
+    public class SupressPermissionErrorsAttribute : Attribute
+    {
+        public readonly bool Supress;
+
+        public SupressPermissionErrorsAttribute(bool supress)
+        {
+            Supress = supress;
+        }
+    }
+}
