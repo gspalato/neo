85c3c9bd2578112d82bd70b20e0def0c6a1c02d8
Hinoki Shiba <3950465+gspalato@users.noreply.github.com>
2020-07-11 16:24:27 -0300

    Multiple fixes.

---
+++ Axion.Core/Commands/Modules/Fun/Clap.cs
@@ -0,0 +1,17 @@
+﻿using Axion.Core.Structures.Attributes;
+using Qmmands;
+using System.Threading.Tasks;
+
+namespace Axion.Core.Commands.Modules.Fun
+{
+	[Category(Category.Fun)]
+	[Group("clap")]
+	public class Clap : AxionModule
+	{
+		[Command]
+		public async Task ExecuteAsync([Name("Text")] [Remainder] string text)
+		{
+			await Context.ReplyAsync(text.Replace(" ", " 👏 "));
+		}
+	}
+}
