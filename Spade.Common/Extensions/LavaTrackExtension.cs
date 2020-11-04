fbed4545b133cff237babcb1f11cedf072b03194
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2020-11-04 16:24:47 -0300

    Update LavaTrackExtension.cs
    - LavaTrackExtension.GenerateSlider() now uses Math.Round to determine the position of the slider;
    - The slider is now an unicode escape sequence;

--- Spade.Common/Extensions/LavaTrackExtension.cs
+++ Spade.Common/Extensions/LavaTrackExtension.cs
@@ -8,13 +8,13 @@ namespace Spade.Common.Extensions
 	{
 		public static string GenerateSlider(this LavaTrack track)
 		{
-			StringBuilder slider = new StringBuilder();
+			StringBuilder slider = new();
 			for (int i = 0; i <= 29; i++)
 				slider.Append("▬");
 
 			double sliderPosition = track.Position.TotalSeconds * 30 / track.Duration.TotalSeconds;
-			int roundSliderPosition = (int)Math.Floor(sliderPosition);
-			slider.Insert((roundSliderPosition <= 0) ? 0 : roundSliderPosition - 1, "🔵");
+			int roundSliderPosition = (int)Math.Round(sliderPosition);
+			slider.Insert((roundSliderPosition <= 0) ? 0 : roundSliderPosition - 1, "\ud83d\udd35");
 
 			return slider.ToString();
 		}
