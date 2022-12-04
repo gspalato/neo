7656b88bad857a8984cda7a894c39c6fd7490742
gspalato <3950465+gspalato@users.noreply.github.com>
2022-12-04 14:34:09 -0300

    Created a Database project with it's services.

---
+++ Oculus.Database/Models/GuildSettings.cs
@@ -0,0 +1,18 @@
+﻿using Postgrest.Attributes;
+using Supabase;
+
+namespace Oculus.Database.Models
+{
+    [Table("guild_settings")]
+    public class GuildSettings : SupabaseModel
+    {
+        [PrimaryKey("guild_id", false)]
+        public string GuildId { get; set; }
+
+        [Column("radio_mode")]
+        public bool isInRadioMode { get; set; }
+
+        [Column("radio_channel_id")]
+        public string? RadioChannelId { get; set; }
+    }
+}
---
+++ Oculus.Database/Services/DatabaseService.cs
@@ -0,0 +1,64 @@
+﻿using Oculus.Database.Models;
+
+namespace Oculus.Database.Services
+{
+    public class DatabaseService
+    {
+        internal Supabase.Client? _client { get; private set; } = null;
+
+        public DatabaseService()
+        {
+
+        }
+
+        public Task InitializeAsync(string url, string key)
+        {
+            if (_client is null)
+                _client = new Supabase.Client(url, key,
+                    new Supabase.SupabaseOptions { AutoConnectRealtime = true });
+
+            return _client.InitializeAsync();
+        }
+
+        public async Task<GuildSettings?> GetGuildSettings(ulong guildId)
+        {
+            if (_client is null)
+                throw new Exception("Not yet connected.");
+
+            var response = await _client.Postgrest.Table<GuildSettings>()
+                .Match(
+                new Dictionary<string, string> {
+                    { "guild_id", guildId.ToString() }
+                })
+                .Get();
+            var matches = response.Models;
+            if (!matches.Any())
+            {
+                return null;
+            }
+
+            return matches.FirstOrDefault();
+        }
+
+        // Returns GuildSettings if successful, returns null if failed.
+        public async Task<GuildSettings?> CreateGuildSettings(ulong guildId)
+        {
+            if (_client is null)
+                throw new Exception("Not yet connected.");
+
+            var settings = new GuildSettings
+            {
+                GuildId = guildId.ToString(),
+                isInRadioMode = false,
+                RadioChannelId = ""
+            };
+
+            var response = await _client.From<GuildSettings>().Insert(settings);
+            Console.WriteLine(response.Content);
+            if (response.ResponseMessage is not null && response.ResponseMessage.IsSuccessStatusCode)
+                return settings;
+            else
+                return null;
+        }
+    }
+}
