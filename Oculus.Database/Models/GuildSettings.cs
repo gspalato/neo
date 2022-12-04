aa5a382d4c8eab2708c7e94f73fd82cd17ad955f
gspalato <3950465+gspalato@users.noreply.github.com>
2022-12-04 15:04:52 -0300

    Fixed GuildSettings Database error.

--- Oculus.Database/Models/GuildSettings.cs
+++ Oculus.Database/Models/GuildSettings.cs
@@ -6,13 +6,13 @@ namespace Oculus.Database.Models
     [Table("guild_settings")]
     public class GuildSettings : SupabaseModel
     {
-        [PrimaryKey("guild_id", false)]
-        public string GuildId { get; set; }
+        [PrimaryKey("guild_id")]
+        public string GuildId { get; set; } = "";
 
         [Column("radio_mode")]
-        public bool isInRadioMode { get; set; }
+        public bool IsInRadioMode { get; set; } = false;
 
         [Column("radio_channel_id")]
-        public string? RadioChannelId { get; set; }
+        public string? RadioChannelId { get; set; } = "";
     }
 }
