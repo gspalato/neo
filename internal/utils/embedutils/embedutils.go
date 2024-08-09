70ad54b6d6be12f8bfcc3415f2143e76847dff7c
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-08-09 16:43:04 -0300

    Created embedutils.

---
+++ internal/utils/embedutils/embedutils.go
@@ -0,0 +1,30 @@
+package embed
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"unreal.sh/neo/internal/utils/static"
+)
+
+func CreateSuccessEmbed(description string) discordgo.MessageEmbed {
+	return discordgo.MessageEmbed{
+		Title:       "✅ **Success**",
+		Color:       0x00FF00,
+		Description: description,
+	}
+}
+
+func CreatePromptEmbed(description string) discordgo.MessageEmbed {
+	return discordgo.MessageEmbed{
+		Title:       "⁉ **Are you sure?**",
+		Color:       static.ColorEmbedGray,
+		Description: description,
+	}
+}
+
+func CreateErrorEmbed(description string) discordgo.MessageEmbed {
+	return discordgo.MessageEmbed{
+		Title:       "⚠ **Error**",
+		Color:       static.ColorEmbedOrange,
+		Description: description,
+	}
+}
