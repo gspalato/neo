4817a7e7050cf322aca31ad4c85b71e945e5b139
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-08-09 16:45:39 -0300

    Created a pause command.

---
+++ internal/commands/slash/pause.go
@@ -0,0 +1,112 @@
+package slash
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/zekrotja/ken"
+	"unreal.sh/neo/internal/services/music"
+)
+
+type PauseCommand struct{}
+
+var (
+	_ ken.SlashCommand       = (*PingCommand)(nil)
+	_ ken.GuildScopedCommand = (*PingCommand)(nil)
+)
+
+func (c *PauseCommand) Name() string {
+	return "pause"
+}
+
+func (c *PauseCommand) Description() string {
+	return "Pauses the current song."
+}
+
+func (c *PauseCommand) Version() string {
+	return "1.0.0"
+}
+
+func (c *PauseCommand) Type() discordgo.ApplicationCommandType {
+	return discordgo.ChatApplicationCommand
+}
+
+func (c *PauseCommand) Options() []*discordgo.ApplicationCommandOption {
+	return []*discordgo.ApplicationCommandOption{}
+}
+
+func (c *PauseCommand) Guild() string {
+	return os.Getenv("MISFITS_GUILD_ID")
+}
+
+func (c *PauseCommand) Run(ctx ken.Context) (err error) {
+	if err = ctx.Defer(); err != nil {
+		return nil
+	}
+
+	session := ctx.GetSession()
+
+	musicService := ctx.Get("MusicService").(*music.MusicService)
+	if musicService == nil {
+		slog.Error("Music service not found in context.")
+		return nil
+	}
+
+	guild, err := ctx.Guild()
+	if err != nil {
+		slog.Error("Failed to fetch guild.")
+		return err
+	}
+
+	var isBotInVoiceChannel bool
+	var botVoiceState *discordgo.VoiceState
+	for _, state := range guild.VoiceStates {
+		if state.UserID == session.State.User.ID {
+			isBotInVoiceChannel = true
+			botVoiceState = state
+		}
+	}
+
+	if !isBotInVoiceChannel {
+		ctx.RespondMessage("I'm not connected to a voice channel.")
+		return nil
+	}
+
+	var voiceChannelID *string
+	for _, state := range guild.VoiceStates {
+		if state.UserID == ctx.User().ID {
+			voiceChannelID = &state.ChannelID
+		}
+	}
+
+	if voiceChannelID == nil {
+		ctx.RespondMessage("You need to be in a voice channel to use this command.")
+		return nil
+	} else if *voiceChannelID != botVoiceState.ChannelID {
+		ctx.RespondMessage("You have to be in the same voice channel as me to use this command.")
+		return nil
+	}
+
+	musicSession := musicService.GetMusicSession(guild.ID)
+	if musicSession == nil {
+		slog.Error("Music session not found. Something's really wrong here.")
+		ctx.RespondMessage("I'm not playing anything right now.")
+		return nil
+	}
+
+	musicSession.Pause()
+
+	/*
+		embed := &discordgo.MessageEmbed{
+			Description: "⏸ **Paused**",
+		}
+
+		err = ctx.RespondEmbed(embed)
+		if err != nil {
+			slog.Error("Failed to respond to command.", err)
+		}
+	*/
+
+	return nil
+}
